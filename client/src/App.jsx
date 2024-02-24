0db698f4c0ae755272e4b6751f2a1f0c2df46b7e
WebDevHarsha <vasriharsha@gmail.com>
2024-02-24 22:27:31 +0530

    updated home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
 
 function App() {
   return (
-    <div>
-      hello
-    </div>
+    <Home></Home>
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+function Home() {
+  return (
+    <div>
+      <div className="flex flex-col md:flex-row">
+        <div className="w-full md:w-1/2 bg-black">
+          <div className="flex flex-col justify-center h-screen md:h-auto">
+            <h1 className="text-white text-center md:text-left text-4xl md:text-6xl">
+              Find the <span className="text-blue-400">Best</span> from around the world.
+            </h1>
+          </div>
+        </div>
+        <div className="w-full md:w-1/2">
+          <img className="w-full" src="./src/assets/bg.gif" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
