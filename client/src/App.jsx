707f9eab7e489ca52672efc0a911575c38c4ec2b
WebDevHarsha <vasriharsha@gmail.com>
2024-02-29 18:10:32 +0530

    added login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <>
-      <Home />
+      {/* <Home /> */}
+      <Login></Login>
       
     </>
   );
