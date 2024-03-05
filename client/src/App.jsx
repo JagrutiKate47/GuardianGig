11d296de209f5d5916ce82c01b3f5864259d72f6
unknown <sidbbp@gmail.com>
2024-03-05 16:54:17 +0530

    asm

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,14 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import BuyerUserP from "./pages/BuyerUserP";
-
+import SellerUserP from "./pages/SellerUserP"
 
 function App() {
   return (
     <>
-      {/* <Home /> */}
-      <BuyerUserP></BuyerUserP>
+      <SellerUserP></SellerUserP>
+      <Home />
+      
       
     </>
   );
